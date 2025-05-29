332422a8ef102f35c4f8c56190153210ac8f8c9c
ako-develop-21 <21saijdpoke@gmail.com>
2025-05-29 21:18:36 +0900

    hashに変更

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import IV0001 from "@/template/IV0001.vue";
 import IV0002 from "@/template/IV0002.vue";
 
 const base = import.meta.env.BASE_URL;
 
 const router = createRouter({
-    history: createWebHistory(base),
+    history: createWebHashHistory(base),
     routes: [
         { component: IV0001, path: "/imageSelect" },
         { component: IV0002, path: "/iconViewer" },
